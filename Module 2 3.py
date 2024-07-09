bd9ba2f721d48026dea8e0ea58da0e52a23a96e5
Vstukan <v.stukan14@gmail.com>
2024-07-09 12:30:11 +0300

    Add files via upload

---
+++ Module 2 3.py
@@ -0,0 +1,19 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list):
+        number = my_list[i]
+        if number > 0:
+            print(number)
+        i = i + 1
+        if number < 0:
+            break
+        continue
+
+
+
+
+
+
+
+
+
