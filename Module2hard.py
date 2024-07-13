50654fd96e5b066b13c78d6e63025b6f1f031a4b
Vstukan <v.stukan14@gmail.com>
2024-07-13 14:19:05 +0300

    Add files via upload

--- Module2hard.py
+++ Module2hard.py
@@ -2,8 +2,12 @@ n = int(input('введите число от 3 до 20: '))
 numbers_p = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 a = 0
 b = 0
+result = []
 for a in numbers_p:
     if a < n:
         for b in numbers_p:
             if b > a and b < n and n % (a + b) == 0:
-                print('result: ', a, b)
+                result.append(a)
+                result.append(b)
+print('result: ', *result)
+
